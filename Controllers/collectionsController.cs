d7a26c85b2df2d1b01f1d9cec2afd28bc5dc198a
vuonghung01 <nnvanloc@gmail.com>
2022-06-13 04:04:39 +0700

    13/6

---
+++ Controllers/collectionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HungManhProject.Controllers
+{
+    public class collectionsController : Controller
+    {
+        // GET: collections
+        public ActionResult all()
+        {
+            return View();
+        }
+    }
+}
