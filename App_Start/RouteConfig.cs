7c7589d2aba2b4a91424c70e59eb918232e60cb1
vuonghung01 <nnvanloc@gmail.com>
2022-06-13 21:33:30 +0700

    13/6 (5) Thêm page about-us, products và thêm footer cho layout

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace HungManhProject
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "AboutUs",
+                url: "pages/about-us",
+                defaults: new { controller = "pages", action = "AboutUs", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Controllers/productsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HungManhProject.Controllers
+{
+    public class productsController : Controller
+    {
+        // GET: products
+        public ActionResult productsName()
+        {
+            return View();
+        }
+    }
+}
